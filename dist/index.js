2057198a7b58bf5afd88a3dcf903bfa828ac533d
Fredsss0 <133476166+Fredsss0@users.noreply.github.com>
2023-05-13 01:35:33 -0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.percentage = void 0;
+function percentage(partialValue, totalValue) {
+    return (100 * partialValue) / totalValue;
+}
+exports.percentage = percentage;
